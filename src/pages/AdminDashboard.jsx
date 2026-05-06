8a96d84a14212a14834fffb038b5fc6571ede981
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-06 16:44:46 +0530

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,4 +1,60 @@
+import {
+  useEffect,
+  useState,
+} from "react";
+
+import {
+  addSubject,
+  deleteSubject,
+  listenSubjects,
+} from "../services/subjectService";
+
 export default function AdminDashboard() {
+
+  const [subjects,
+    setSubjects] =
+    useState([]);
+
+  const [title,
+    setTitle] =
+    useState("");
+
+  const [description,
+    setDescription] =
+    useState("");
+
+  const [duration,
+    setDuration] =
+    useState("");
+
+  useEffect(() => {
+
+    const unsubscribe =
+      listenSubjects(
+        setSubjects
+      );
+
+    return () =>
+      unsubscribe();
+
+  }, []);
+
+  async function createSubject() {
+
+    if (!title) return;
+
+    await addSubject({
+      title,
+      description,
+      duration,
+      negativeMarking:0.25,
+    });
+
+    setTitle("");
+    setDescription("");
+    setDuration("");
+
+  }
 
   return (
 
@@ -8,19 +64,87 @@ export default function AdminDashboard() {
         Admin Dashboard
       </h1>
 
+      <div className="card">
+
+        <h2>
+          Add Subject
+        </h2>
+
+        <input
+          placeholder="Subject Title"
+          value={title}
+          onChange={(e)=>
+            setTitle(
+              e.target.value
+            )
+          }
+        />
+
+        <input
+          placeholder="Description"
+          value={description}
+          onChange={(e)=>
+            setDescription(
+              e.target.value
+            )
+          }
+        />
+
+        <input
+          placeholder="Duration"
+          value={duration}
+          onChange={(e)=>
+            setDuration(
+              e.target.value
+            )
+          }
+        />
+
+        <button
+          onClick={
+            createSubject
+          }
+        >
+          Add Subject
+        </button>
+
+      </div>
+
       <div className="grid">
 
-        <div className="card">
-          Add Subject
-        </div>
+        {subjects.map((s) => (
 
-        <div className="card">
-          Add Questions
-        </div>
+          <div
+            key={s.id}
+            className="subject-card"
+          >
 
-        <div className="card">
-          Analytics
-        </div>
+            <h2>
+              {s.title}
+            </h2>
+
+            <p>
+              {s.description}
+            </p>
+
+            <p>
+              Duration:
+              {s.duration} mins
+            </p>
+
+            <button
+              onClick={() =>
+                deleteSubject(
+                  s.id
+                )
+              }
+            >
+              Delete
+            </button>
+
+          </div>
+
+        ))}
 
       </div>
 

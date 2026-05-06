f2be7efd8112ed14a881813465e41b62c9b4b85e
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-07 02:22:16 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./styles/globals.css";
+
+import { Toaster } from "react-hot-toast";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+
+    <App />
+
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        style: {
+          background: "#111827",
+          color: "#f8fafc",
+          border: "1px solid #1e293b",
+        },
+      }}
+    />
+
+  </React.StrictMode>
+);   

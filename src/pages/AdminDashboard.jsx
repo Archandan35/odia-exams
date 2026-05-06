e2c228869ca0c38853d320d5a607a0bb8a3a4985
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-06 15:33:56 +0530

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,1 +1,29 @@
-export default function Component(){return <div className='card'>Component</div>}
+export default function AdminDashboard() {
+
+  return (
+
+    <div className="page">
+
+      <h1>
+        Admin Dashboard
+      </h1>
+
+      <div className="grid">
+
+        <div className="card">
+          Add Subject
+        </div>
+
+        <div className="card">
+          Add Questions
+        </div>
+
+        <div className="card">
+          Analytics
+        </div>
+
+      </div>
+
+    </div>
+  );
+}

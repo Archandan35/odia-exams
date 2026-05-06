061d214b7b412a70e83ae5f99651d27c5e0d44b6
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-06 15:33:30 +0530

    Update StudentDashboard.jsx

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,1 +1,47 @@
-export default function Component(){return <div className='card'>Component</div>}
+export default function StudentDashboard() {
+
+  return (
+
+    <div className="page">
+
+      <h1>
+        Student Dashboard
+      </h1>
+
+      <div className="grid">
+
+        <div className="subject-card">
+
+          <h2>SBI PO Mock</h2>
+
+          <button>
+            Start Test
+          </button>
+
+        </div>
+
+        <div className="subject-card">
+
+          <h2>RRB PO Mock</h2>
+
+          <button>
+            Start Test
+          </button>
+
+        </div>
+
+        <div className="subject-card">
+
+          <h2>SSC Mock</h2>
+
+          <button>
+            Start Test
+          </button>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}

33986a860c728247c3e2bf0096fb07235982d287
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-06 16:43:40 +0530

    Update subjectService.js

--- src/services/subjectService.js
+++ src/services/subjectService.js
@@ -1,1 +1,53 @@
-export default function Component(){return <div className='card'>Component</div>}
+import {
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+
+import {
+  db,
+} from "../firebase/config";
+
+const subjectsRef =
+  collection(db, "subjects");
+
+export async function addSubject(
+  subject
+) {
+
+  await addDoc(
+    subjectsRef,
+    subject
+  );
+}
+
+export async function deleteSubject(
+  id
+) {
+
+  await deleteDoc(
+    doc(db, "subjects", id)
+  );
+}
+
+export function listenSubjects(
+  callback
+) {
+
+  return onSnapshot(
+    subjectsRef,
+    (snapshot) => {
+
+      const data =
+        snapshot.docs.map((d) => ({
+          id: d.id,
+          ...d.data(),
+        }));
+
+      callback(data);
+
+    }
+  );
+}

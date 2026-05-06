beb19a2f0187ea9d7d93bd8f4ad92cdcda9507de
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-06 23:47:15 +0530

    Update AdminPage.jsx

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,5 +1,8 @@
-import Dashboard from "../admin/Dashboard";
+import Dashboard
+from "../admin/Dashboard";
 
-export default function AdminPage() {
-  return <Dashboard />;
+export default function AdminPage(){
+
+return <Dashboard/>;
+
 }

ba14af78dfe8c45743f5e520396f9a4d5630c530
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-07 02:18:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,29 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
 import "./styles/globals.css";
 
-import {
-Toaster,
-} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(
-document.getElementById("root")
+  document.getElementById("root")
 ).render(
+  <React.StrictMode>
 
-<React.StrictMode>
+    <App />
 
-<App />
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        style: {
+          background: "#111827",
+          color: "#f8fafc",
+          border: "1px solid #1e293b",
+        },
+      }}
+    />
 
-<Toaster
-position="top-right"
-toastOptions={{
-
-style:{
-
-background:"#111827",
-
-color:"#f8fafc",
-
-border:
-"1px solid #1e293b",
-
-},
-
-}}
-
- />
-
-</React.StrictMode>
-
+  </React.StrictMode>
 );

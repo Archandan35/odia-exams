d367a1fb76a3ccf2906eab54144a0bf6708a652b
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-06 15:58:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,97 @@
-import {Routes,Route} from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/StudentDashboard';
-import AdminDashboard from './pages/AdminDashboard';
-import ExamPage from './pages/ExamPage';
-export default function App(){return <Routes><Route path='/' element={<Login/>}/><Route path='/dashboard' element={<Dashboard/>}/><Route path='/admin' element={<AdminDashboard/>}/><Route path='/exam' element={<ExamPage/>}/></Routes>}
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Login from "./pages/Login";
+
+import Register from "./pages/Register";
+
+import Dashboard from "./pages/StudentDashboard";
+
+import AdminDashboard from "./pages/AdminDashboard";
+
+import ExamPage from "./pages/ExamPage";
+
+import AnalyticsPage from "./pages/AnalyticsPage";
+
+import LeaderboardPage from "./pages/LeaderboardPage";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+import AdminRoute from "./routes/AdminRoute";
+
+export default function App() {
+
+  return (
+
+    <Routes>
+
+      <Route
+        path="/"
+        element={<Login />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+
+            <Dashboard />
+
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/exam"
+        element={
+          <ProtectedRoute>
+
+            <ExamPage />
+
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/leaderboard"
+        element={
+          <ProtectedRoute>
+
+            <LeaderboardPage />
+
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/analytics"
+        element={
+          <AdminRoute>
+
+            <AnalyticsPage />
+
+          </AdminRoute>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <AdminRoute>
+
+            <AdminDashboard />
+
+          </AdminRoute>
+        }
+      />
+
+    </Routes>
+  );
+}

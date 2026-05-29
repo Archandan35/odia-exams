4c33e405c7ef2d726a6cb998fedc6fde4fc3403a
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-29 18:06:37 +0530

    Update ExamPage.jsx

--- src/pages/ExamPage.jsx
+++ src/pages/ExamPage.jsx
@@ -1,5 +1,42 @@
-import ExamPageDesktop from "./ExamPageDesktop";
+import { useEffect, useState }
+from "react";
+
+import ExamPageDesktop
+from "./ExamPageDesktop";
+
+import ExamPagePhone
+from "./ExamPagePhone";
 
 export default function ExamPage() {
-  return <ExamPageDesktop />;
+
+  const [isMobile,setIsMobile] =
+    useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+
+    const resize = () => {
+
+      setIsMobile(
+        window.innerWidth <= 768
+      );
+
+    };
+
+    window.addEventListener(
+      "resize",
+      resize
+    );
+
+    return () =>
+      window.removeEventListener(
+        "resize",
+        resize
+      );
+
+  }, []);
+
+  return isMobile
+    ? <ExamPagePhone />
+    : <ExamPageDesktop />;
+
 }

28f6b9a2c31569c78adb44a14a2a3992d970ac2f
Archandan35 <88570404+Archandan35@users.noreply.github.com>
2026-05-29 17:16:49 +0530

    Create useExamEngine.js

---
+++ src/hooks/useExamEngine.js
@@ -0,0 +1,28 @@
+return {
+  loading,
+  questions,
+  examData,
+
+  currentQuestion,
+  setCurrentQuestion,
+
+  answers,
+  visited,
+  review,
+  bookmarks,
+
+  selectOption,
+  handleNext,
+  handlePrev,
+
+  clearResponse,
+  toggleReview,
+  toggleBookmark,
+
+  submitExam,
+
+  timeLeft,
+  cheatCount,
+
+  formatTime
+};
